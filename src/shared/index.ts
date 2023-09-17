6cda63b63c058b1b769a71be588733628434a5ea
jianchuxin <903243301@qq.com>
2023-09-17 11:16:30 +0800

    实现代码生成三种联合类型

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,15 +1,18 @@
 export const extend = Object.assign;
 
 export const EMPTY_OBJ = {};
+
 export function isObject(value) {
     return value !== null && typeof value === "object";
 }
+
+export const isString = (value) => typeof value === "string";
+
 export function hasChanged(newValue, value) {
     return !Object.is(newValue, value);
 }
 
-export const hasOwn = (obj, key) =>
-    Object.prototype.hasOwnProperty.call(obj, key);
+export const hasOwn = (obj, key) => Object.prototype.hasOwnProperty.call(obj, key);
 
 export function camelize(str: string) {
     // 转为驼峰表示 add-one -> addOne
@@ -17,10 +20,12 @@ export function camelize(str: string) {
         return c ? c.toUpperCase() : "";
     });
 }
+
 function capitalize(str: string) {
     // 首字母大写
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
 export function toHandlerKey(str: string) {
     // on +
     return str ? "on" + capitalize(str) : "";
